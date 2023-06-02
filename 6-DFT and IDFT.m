2fa6b97411ca91e3372008034f9b0673621f4f1d
Abhirath Sujith <78019581+abhirathsujith@users.noreply.github.com>
2023-06-02 09:42:12 +0530

    Update 6-DFT and IDFT.m

--- 6-DFT and IDFT.m
+++ 6-DFT and IDFT.m
@@ -7,8 +7,26 @@ X = fft(x);
 % Compute the IDFT
 reconstructed_x = ifft(X);
 
-% Display the results
-disp('DFT:');
-disp(X);
-disp('IDFT:');
-disp(reconstructed_x);
+% Plot the original signal
+subplot(3, 1, 1);
+stem(0:length(x)-1, x, 'filled');
+xlabel('n');
+ylabel('x(n)');
+title('Original Signal');
+
+% Plot the magnitude spectrum (DFT)
+subplot(3, 1, 2);
+stem(0:length(X)-1, abs(X), 'filled');
+xlabel('k');
+ylabel('|X(k)|');
+title('Magnitude Spectrum (DFT)');
+
+% Plot the reconstructed signal (IDFT)
+subplot(3, 1, 3);
+stem(0:length(reconstructed_x)-1, real(reconstructed_x), 'filled');
+xlabel('n');
+ylabel('x(n)');
+title('Reconstructed Signal (IDFT)');
+
+% Adjust the spacing between subplots
+sgtitle('DFT and IDFT');
