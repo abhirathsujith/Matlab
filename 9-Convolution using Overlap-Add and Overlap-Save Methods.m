fbd84007c9410f42cc58ecda687223bef6cc11f0
Abhirath Sujith <78019581+abhirathsujith@users.noreply.github.com>
2023-06-02 10:10:23 +0530

    Update 9-Convolution using Overlap-Add and Overlap-Save Methods.m

--- 9-Convolution using Overlap-Add and Overlap-Save Methods.m
+++ 9-Convolution using Overlap-Add and Overlap-Save Methods.m
@@ -1,38 +1,25 @@
-% Define the input sequences
-x = [1, 2, 3, 4];       % Sequence 1
-h = [1, -1, 1, -1];     % Sequence 2
+% Define the input signals and filter impulse response
+x = [0.2, 0.4, 0.6, 0.8, 1.0];  % Input signal
+h = [0.1, 0.2, 0.1];             % Filter impulse response
 
-% Define the block size and overlap size
-blockSize = 2;          % Block size for overlap-add/save
-overlapSize = 1;        % Overlap size for overlap-add/save
+% Define the block size and hop size for overlap-add
+blockSize = length(h);
+hopSize = blockSize;
 
-% Perform convolution using overlap-add method
-yOverlapAdd = overlapAddConvolution(x, h, blockSize, overlapSize);
+% Pad the input signal with zeros to make its length divisible by the block size
+x = [x, zeros(1, blockSize - mod(length(x), blockSize))];
 
-% Perform convolution using overlap-save method
-yOverlapSave = overlapSaveConvolution(x, h, blockSize, overlapSize);
+% Perform convolution using the overlap-add method
+output = zeros(1, length(x) + length(h) - 1);  % Initialize the output signal
 
-% Plot the original signals
-subplot(3, 1, 1);
-stem(0:length(x)-1, x, 'filled');
-xlabel('n');
-ylabel('x(n)');
-title('Sequence 1');
+for i = 1:hopSize:length(x)
+    inputBlock = x(i:i+blockSize-1);                   % Extract a block of input signal
+    outputBlock = conv(inputBlock, h);                 % Convolve the input block with the filter impulse response
+    output(i:i+length(outputBlock)-1) = output(i:i+length(outputBlock)-1) + outputBlock;  % Overlap-add the output blocks
+end
 
-subplot(3, 1, 2);
-stem(0:length(h)-1, h, 'filled');
-xlabel('n');
-ylabel('h(n)');
-title('Sequence 2');
-
-% Plot the output signals (convolutions)
-subplot(3, 1, 3);
-hold on;
-stem(0:length(yOverlapAdd)-1, yOverlapAdd, 'filled', 'DisplayName', 'Overlap-Add');
-stem(0:length(yOverlapSave)-1, yOverlapSave, 'filled', 'DisplayName', 'Overlap-Save');
-hold off;
-xlabel('n');
-ylabel('y(n)');
-title('Convolution');
-legend('Location', 'best');
-
+% Plot the output signal
+stem(0:length(output)-1, output, 'filled');
+title('Convolution using Overlap-Add');
+xlabel('Sample');
+ylabel('Amplitude');
