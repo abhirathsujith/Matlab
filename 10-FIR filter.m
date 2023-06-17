87f27d292572b550217f3fb1134aacb611a54459
Abhirath Sujith <78019581+abhirathsujith@users.noreply.github.com>
2023-06-17 11:25:08 +0530

    Update 10-FIR filter.m

--- 10-FIR filter.m
+++ 10-FIR filter.m
@@ -1,33 +1,45 @@
-% Filter specifications
-cutoff_freq = 0.2; % Normalized cutoff frequency (0 to 1)
-filter_order = 50; % Filter order
 
-% Determine window type and length
-window_type = @hamming;
-window_length = filter_order + 1;
+% FIR Filter Design using Window Techniques
 
-% Design filter coefficients
-filter_coeffs = fir1(filter_order, 2*cutoff_freq, window_type);
+% Filter Specifications
+fs = 1000; % Sampling frequency (Hz)
+fpass = 100; % Passband edge frequency (Hz)
+fstop = 200; % Stopband edge frequency (Hz)
+apass = 1; % Passband ripple (dB)
+astop = 60; % Stopband attenuation (dB)
 
-% Generate input signal
-Fs = 1000; % Sampling frequency
-t = 0:1/Fs:1; % Time vector
-f = 10; % Signal frequency
-input_signal = sin(2*pi*f*t);
+% Filter Order
+N = 101; % Filter order (odd number)
 
-% Apply the filter
-output_signal = filter(filter_coeffs, 1, input_signal);
+% Compute the number of elements for the ideal response arrays
+passband_len = round(fpass/(fs/2));
+stopband_len = round((fstop - fpass)/(fs/2));
 
-% Plot the input and output signals
+% Create ideal frequency response
+ideal_resp = [ones(1, passband_len), zeros(1, stopband_len)];
+ideal_resp = [ideal_resp, fliplr(ideal_resp)];
+
+% Design FIR filter using the windowing technique
+filter_coeffs = fir1(N-1, fstop/(fs/2), 'low', kaiser(N, 4));
+
+% Compute actual frequency response of the designed filter
+freq_resp = freqz(filter_coeffs, 1, 1024, fs);
+
+% Plot the frequency response
 figure;
-subplot(2, 1, 1);
-plot(t, input_signal);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Input Signal');
+subplot(2,1,1);
+plot(linspace(0, fs/2, 512), 20*log10(abs(freq_resp(1:512))));
+hold on;
+plot(linspace(0, fs/2, length(ideal_resp)), 20*log10(abs(ideal_resp)), 'r--');
+title('Frequency Response of FIR Filter');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+legend('Actual Response', 'Ideal Response');
+grid on;
 
-subplot(2, 1, 2);
-plot(t, output_signal);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Output Signal');
+subplot(2,1,2);
+plot(linspace(0, fs/2, 512), unwrap(angle(freq_resp(1:512))));
+title('Phase Response of FIR Filter');
+xlabel('Frequency (Hz)');
+ylabel('Phase (rad)');
+grid on;
